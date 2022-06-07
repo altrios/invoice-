3142bfc5577261b2d52d2f8d7e37c15e640f8dfe
altrios <famartinez.ti@gmail.com>
2022-06-07 17:55:02 +0200

    add token validation to companyRef

--- companyref/src/Controllers/companyRef.controller.js
+++ companyref/src/Controllers/companyRef.controller.js
@@ -1,69 +1,153 @@
-const CompanyRef = require('../models/companyRef.model');
+const CompanyRef = require('../models/companyRef.model')
 
 exports.create = function (req, res) {
-    const new_company = new CompanyRef(req.body);
+  const new_company = new CompanyRef(req.body)
+  const tokenIsOk = testToken(req.body.token)
+  if (tokenIsOk == true) {
     CompanyRef.create(new_company, function (err, companyRef) {
-        if (err) {
-            res.status(500).json(err);
-		} else {
-			res.status(200).json({
-				error: false,
-				message: "User added to the company successfully!",
-				data: companyRef
-			});
-		}
-    });
-};
+      if (err) {
+        res.status(500).json(err)
+      } else {
+        res.status(200).json({
+          error: false,
+          message: 'User added to the company successfully!',
+          data: companyRef,
+        })
+      }
+    })
+  } else {
+    res.status(500).send({
+      error: true,
+      message: 'Error en token, vuelva a iniciar sesión',
+    })
+  }
+}
 
 exports.findById = function (req, res) {
-	CompanyRef.findById(req.params.id, function (err, refer) {
-		if (err) {
-			res.status(500).send(err);
-		} else {
-			res.status(200).send(refer);
-		}
-	});
-};
+  const tokenIsOk = testToken(req.body.token)
+  if (tokenIsOk == true) {
+    CompanyRef.findById(req.params.id, function (err, refer) {
+      if (err) {
+        res.status(500).send(err)
+      } else {
+        res.status(200).send(refer)
+      }
+    })
+  } else {
+    res.status(500).send({
+      error: true,
+      message: 'Error en token, vuelva a iniciar sesión',
+    })
+  }
+}
 
 exports.findCompaniesByUserId = function (req, res) {
-	CompanyRef.findCompaniesByUserId(req.params.id, function (err, companies) {
-		if (err) {
-			res.status(500).send(err);
-		} else {
-			res.status(200).send(companies);
-		}
-	});
-};
+  const tokenIsOk = testToken(req.body.token)
+  if (tokenIsOk == true) {
+    CompanyRef.findCompaniesByUserId(req.params.id, function (err, companies) {
+      if (err) {
+        res.status(500).send(err)
+      } else {
+        res.status(200).send(companies)
+      }
+    })
+  } else {
+    res.status(500).send({
+      error: true,
+      message: 'Error en token, vuelva a iniciar sesión',
+    })
+  }
+}
 
 exports.findUsersByCompanyId = function (req, res) {
-	CompanyRef.findUsersByCompanyId(req.params.id, function (err, users) {
-		if (err) {
-			res.status(500).send(err);
-		} else {
-			res.status(200).send(users);
-		}
-	});
-};
+  const tokenIsOk = testToken(req.body.token)
+  if (tokenIsOk == true) {
+    CompanyRef.findUsersByCompanyId(req.params.id, function (err, users) {
+      if (err) {
+        res.status(500).send(err)
+      } else {
+        res.status(200).send(users)
+      }
+    })
+  } else {
+    res.status(500).send({
+      error: true,
+      message: 'Error en token, vuelva a iniciar sesión',
+    })
+  }
+}
 
 exports.update = function (req, res) {
-	CompanyRef.update(req.params.id, new CompanyRef(req.body), function (err, user) {
-		if (err)
-			res.status(500).send(err);
-		res.status(200).json({
-			error: false,
-			message: 'Company successfully updated'
-		});
-	});
-
-};
+  const tokenIsOk = testToken(req.body.token)
+  if (tokenIsOk == true) {
+    CompanyRef.update(req.params.id, new CompanyRef(req.body), function (
+      err,
+      user,
+    ) {
+      if (err) res.status(500).send(err)
+      res.status(200).json({
+        error: false,
+        message: 'Company successfully updated',
+      })
+    })
+  } else {
+    res.status(500).send({
+      error: true,
+      message: 'Error en token, vuelva a iniciar sesión',
+    })
+  }
+}
 
 exports.delete = function (req, res) {
-	CompanyRef.delete(req.params.id, function (err, user) {
-		if (err)
-			res.status(500).send(err);
-		res.status(200).json({
-			error: false,
-			message: 'Company successfully deleted'
-		});
-	});
-};
+  const tokenIsOk = testToken(req.body.token)
+  if (tokenIsOk == true) {
+    CompanyRef.delete(req.params.id, function (err, user) {
+      if (err) res.status(500).send(err)
+      res.status(200).json({
+        error: false,
+        message: 'Company successfully deleted',
+      })
+    })
+  } else {
+    res.status(500).send({
+      error: true,
+      message: 'Error en token, vuelva a iniciar sesión',
+    })
+  }
+}
+
+function testToken(token) {
+  var axios = require('axios')
+  var result = ''
+  User.endpoint('verficate_token', (err, data) => {
+    if (err) {
+      console.log(err)
+      result = err
+    } else {
+      url = JSON.parse(data[0]['url'])
+
+      var data = JSON.stringify({
+        token: token,
+      })
+
+      var config = {
+        method: 'post',
+        url: url.endpoint,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        data: data,
+      }
+
+      axios(config)
+        .then(function (response) {
+          console.log(JSON.stringify(response.data))
+        })
+        .catch(function (error) {
+          console.log(error)
+        })
+    }
+  })
+
+  return result
+}
