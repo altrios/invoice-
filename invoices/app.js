4d4b166e7dcb78a979699137ba5de6f43655f2eb
PeepoGeek <101522847+PeepoGeek@users.noreply.github.com>
2022-07-26 13:17:10 -0400

    Separar Server de App.js , conexion a Atlas

--- invoices/app.js
+++ invoices/app.js
@@ -1,29 +1,27 @@
-const express = require('express');
+const express = require("express");
 
 var fileupload = require("express-fileupload");
-
-
-// Setup server port
-const port = process.env.PORT || 5003;
+// // Setup server port
+// const port = process.env.PORT || 5003;
 // create express app
-const app = express()
-const cors = require('cors');
-// parse requests of content-type - application/x-www-form-urlencoded
-app.use(express.urlencoded({ extended: true }))
-// parse requests of content-type - application/json
-app.use(express.json())
+const app = express();
+const cors = require("cors");
+//parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+//parse requests of content-type - application/json
+app.use(express.json());
 app.use(cors());
 app.use(fileupload());
 
-
-app.get('/', (req, res) => {
-    res.status(200).send({ status: "Server for 'invoicePlus' is works!"});
+app.get("/", (req, res) => {
+  res.status(200).send({ status: "Server for 'invoicePlus' is works!" });
 });
 
-app.use('/api/v1/invoice', require('./src/routes/invoice.routes'));
+app.use("/api/v1/invoice", require("./src/routes/invoice.routes"));
 
+module.exports = app;
 
-// listen for requests
-app.listen(port, () => {
-    console.log("Server is listening on port " + port);
-});
+// // listen for requests
+// app.listen(port, () => {
+//     console.log("Server is listening on port " + port);
+// });
---
+++ invoices/server.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
+
+const app = require("./app");
+
+const DB = process.env.DATABASE.replace(
+  "<password>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose.connect(DB, {}).then(() => console.log("DB connection successful!"));
+
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
+
+process.on("unhandledRejection", (err) => {
+  console.log("UNHANDLED REJECTION! 💥 Shutting down...");
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
